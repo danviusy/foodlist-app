cf9783ba1ed8970cd92d4e955813a89186aa6006
danviusy <68064479+danviusy@users.noreply.github.com>
2025-12-07 02:01:04 +0100

    Initial React project setup + API-fetching

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://www.themealdb.com/api/json/v1/1/random.php"
+        );
+        const data = await response.json();
+        const meal = data.meals[0];
+        console.log("Fetched data:", data);
+        console.log("Random Meal:", meal.strMeal);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
